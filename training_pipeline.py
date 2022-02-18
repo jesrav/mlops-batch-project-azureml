fdf62dda6335a5b31dbb15a4e07a9d0bf376b45a
Jes Ravnbøl <jesravnbol@hotmail.com>
2022-02-18 19:25:44 +0000

    wip

--- training_pipeline.py
+++ training_pipeline.py
@@ -26,9 +26,6 @@ workspace = Workspace.get(
 
 experiment=Experiment(workspace=workspace, name="housing-model-training-pipeline")
 
-
-
-# Default datastore 
 datastore = workspace.get_default_datastore()
 
 compute_target = workspace.compute_targets["cpu-cluster"]
@@ -36,21 +33,58 @@ compute_target = workspace.compute_targets["cpu-cluster"]
 aml_run_config = RunConfiguration()
 aml_run_config.environment = Environment.get(workspace=workspace, name="mlops-example-proj-env")
 
-entry_point = "src/data/get_raw_data.py"
-
-output_data1 = OutputFileDatasetConfig(destination = (datastore, 'outputdataset/{run-id}'))
-output_data_dataset = output_data1.register_on_complete(name = 'raw_training_data')
-
+################################################
+# Get raw data step
+################################################
+raw_training_data = OutputFileDatasetConfig()
+raw_training_data_registered = raw_training_data.register_on_complete(name = 'raw_training_data')
 
 get_raw_data_step = PythonScriptStep(
-    script_name=entry_point,
-    source_directory=".",
-    arguments=["data.raw_data", output_data1],
-    compute_target=compute_target,
-    runconfig=aml_run_config,
-    allow_reuse=True
+   script_name="src/data/get_raw_data.py",
+   source_directory=".",
+   arguments=[f"data.raw_data={raw_training_data.arg_val}"],
+   outputs=[raw_training_data],
+   compute_target=compute_target,
+   runconfig=aml_run_config,
+   allow_reuse=True
 )
 
+
+################################################
+# Preprocess data step
+################################################
+preprocesed_training_data = OutputFileDatasetConfig()
+preprocesed_training_data_registered = preprocesed_training_data.register_on_complete(
+    name = 'preprocesed_training_data'
+)
+
+preproces_training_data_step = PythonScriptStep(
+   script_name="src/data/process_data.py",
+   source_directory=".",
+   arguments=[f"data.clean_data={preprocesed_training_data.arg_val}"],
+   outputs=[preprocesed_training_data],
+   compute_target=compute_target,
+   runconfig=aml_run_config,
+   allow_reuse=True
+)
+
+################################################
+# Add features step
+################################################
+preprocesed_training_data = OutputFileDatasetConfig()
+preprocesed_training_data_registered = preprocesed_training_data.register_on_complete(
+    name = 'preprocesed_training_data'
+)
+
+preproces_training_data_step = PythonScriptStep(
+   script_name="src/data/process_data.py",
+   source_directory=".",
+   arguments=[f"data.clean_data={preprocesed_training_data.arg_val}"],
+   outputs=[preprocesed_training_data],
+   compute_target=compute_target,
+   runconfig=aml_run_config,
+   allow_reuse=True
+)
 test_pipeline = Pipeline(workspace=workspace, steps=[get_raw_data_step])
 
 test_pipeline_run = Experiment(workspace, 'test_pipeline_exp').submit(test_pipeline)
