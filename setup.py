15b65b943bebd0daf361802851b3a68cfdd379b3
Jes Ravnbøl <jesravnbol@hotmail.com>
2022-03-01 12:02:29 +0000

    wip

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import find_packages
-from setuptools import setup
-
-setup(
-    name="src",
-    version="0.0.1",
-    description="Training pipeline.",
-    packages=find_packages(),
-)
